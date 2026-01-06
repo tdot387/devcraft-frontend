eeae47bb3b717e281cf564fc4e08e028f4ce0a28
Thomas Wagner <t-wagner@web.de>
2026-01-06 19:30:40 +0100

    added bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 // App-Startpunkt (initialisiert Router & Views)
 import './assets/styles/main.css';
 import { initRouter } from './core/router';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 // TODO: App initialisieren
 initRouter();

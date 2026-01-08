1649ea8a554ce1be6bb12fa1a4d01cca3d68e9cf
Mike <mike.miller0295@gmail.com>
2026-01-08 13:54:17 +0100

    [FDDA19] - create# layout template

--- src/layouts/default.layouts.ts
+++ src/layouts/default.layouts.ts
@@ -4,7 +4,10 @@ import { renderFooter } from '../components/footer';
 export function renderDefaultLayout() {
   const header = document.querySelector('#header')!;
   const footer = document.querySelector('#footer')!;
-  
+  const app = document.querySelector('#app')!;
+
+  app.className = 'flex-grow-1 container';
+
   header.innerHTML = renderHeader();
   footer.innerHTML = renderFooter();
 }
--- src/layouts/layoutManager.ts
+++ src/layouts/layoutManager.ts
@@ -3,11 +3,23 @@ type LayoutRenderer = () => void;
 
 const layouts: Record<string, LayoutRenderer> = {
   default: renderDefaultLayout,
+  auth: () => {
+    console.log('Render auth layout');
+    const header = document.querySelector('#header')!;
+    const footer = document.querySelector('#footer')!;
+    const app = document.querySelector('#app')!;
+    
+    // Set auth layout classes
+    app.className = 'flex-grow-1 d-flex justify-content-center align-items-center';
+    
+    header.innerHTML = '<div class="bg-warning p-3 text-center">🔐 AUTH HEADER</div>';
+    footer.innerHTML = '<div class="bg-info p-2 text-center mt-auto">Auth Footer</div>';
+  },
 };
 
 const routeLayouts: Record<string, string> = {
   '/': 'default',
-  '/about': 'default',
+  '/about': 'auth',
   '/recipe': 'default',
 };
 

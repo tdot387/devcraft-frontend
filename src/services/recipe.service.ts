87d2460dc6a8124cb07f05251c2213023f2ec248
Thomas Wagner <t-wagner@web.de>
2026-01-08 15:38:18 +0100

    adding new recipes with dummy data

--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 import { db } from '@/services/firebase/firebaseApp';
 import type { IRecipeWithId } from '@/types/recipe.types';
 
@@ -9,3 +9,19 @@ export async function getRecipes(): Promise<IRecipeWithId[]> {
     ...(doc.data() as Omit<IRecipeWithId, 'id'>),
   }));
 }
+
+export async function createRecipe() {
+
+  try {
+    const docRef = await addDoc(collection(db, 'recipes'), {
+      name: 'Test'
+    });
+    console.log('Success!', docRef.id);
+
+  }
+  catch (e) {
+    console.log('that didnt work', e);
+
+  }
+}
+

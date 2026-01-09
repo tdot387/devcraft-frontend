0e9fce7c25f6af82a8431d1e6fc0ca2536c01144
Thomas Wagner <t-wagner@web.de>
2026-01-09 14:50:44 +0100

    * recipe can now be transfered to firebase

--- src/views/addNewRecipeView.ts
+++ src/views/addNewRecipeView.ts
@@ -1,50 +1,102 @@
 import { createRecipe } from "@/services/recipe.service";
 import { renderAddNewRecipeTemplate } from "@/templates/addNewRecipe.template";
-import type { IRecipe } from "@/types/recipe.types";
+import type { IRecipe, IIngredient, TUnit } from "@/types/recipe.types";
 
 export function renderAddNewRecipeView() {
   const app = document.querySelector('#app')!;
   app.innerHTML = renderAddNewRecipeTemplate();
 
-  let newRecipeNameInput = document.getElementById('new-recipe-name') as HTMLInputElement;
-  let newRecipeDescriptionInput = document.getElementById('new-recipe-description') as HTMLInputElement;
-  let newRecipeCategoryInput = document.getElementById('new-recipe-category') as HTMLFormElement;
-  let newRecipeIngredientsInput = document.getElementById('new-recipe-ingredient') as HTMLFormElement;
-  let newRecipeForm = document.getElementById('recipe-form') as HTMLFormElement;
-  let newRecipeImgUrl = document.getElementById('new-recipe-image-url') as HTMLFormElement;
+  const newRecipeNameInput = document.getElementById('new-recipe-name') as HTMLInputElement;
+  const newRecipeDescriptionInput = document.getElementById('new-recipe-description') as HTMLInputElement;
+  const newRecipeCategoryInput = document.getElementById('new-recipe-category') as HTMLInputElement;
+  const newlyAddedCategories = document.getElementById('newly-added-categories') as HTMLElement;
+  const addCategoryBtn = document.getElementById('add-new-recipe-btn') as HTMLButtonElement;
+  const newRecipeIngredientsInputName = document.getElementById('new-recipe-ingr-name') as HTMLInputElement;
+  const newRecipeIngredientsInputAmount = document.getElementById('new-recipe-ingr-amount') as HTMLInputElement;
+  const newRecipeIngredientsInputUnit = document.getElementById('new-recipe-ingr-unit') as HTMLInputElement;
+  const newlyAddedIngredients = document.getElementById('newly-added-ingredients') as HTMLElement;
+  const addIngredientBtn = document.getElementById('add-ingr-btn') as HTMLButtonElement;
+  const newRecipeForm = document.getElementById('recipe-form') as HTMLFormElement;
+  const newRecipeImgUrl = document.getElementById('new-recipe-image-url') as HTMLInputElement;
+  const newRecipeInstructionsInput = document.getElementById('new-recipe-instructions') as HTMLTextAreaElement;
 
-  
   let newRecipeCategory: string[] = [];
+  let newRecipeIngredients: IIngredient[] = [];
+
+  /*** 
+   * These two functions push a new category into the newRecipeCategory array
+   * and then display the newly added category below the category input field
+   * 
+   */
 
   newRecipeCategoryInput.addEventListener('keydown', (event) => {
     if (event.key !== 'Enter') return;
-
     event.preventDefault();
-    
+
+    addCategoryToArray();
+  });
+
+  let addCategoryToArray = () => {
     const value = newRecipeCategoryInput.value.trim();
-    if(!value) return;
+    if (!value) return;
 
     newRecipeCategory.push(value);
-
-    console.log('This is your category:', newRecipeCategoryInput.value)
     newRecipeCategoryInput.value = '';
 
     showNewlyAddedCategories();
+  }
 
-  })
+  addCategoryBtn.addEventListener('click', () => {
+    addCategoryToArray();
+  });
 
   let showNewlyAddedCategories = () => {
-    const newlyAddedCategories = document.getElementById('newly-added-categories') as HTMLElement;
-    
     newlyAddedCategories.textContent = '';
 
     for (let category of newRecipeCategory) {
       const cat = document.createElement('span');
       cat.textContent = category;
-      cat.classList.add('badge', 'text-bg-primary', 'me-2')
+      cat.classList.add('badge', 'text-bg-success', 'me-2')
       newlyAddedCategories.appendChild(cat);
     }
+  }
 
+
+  let addIngredientToArray = () => {
+    const nameValue = newRecipeIngredientsInputName.value.trim();
+    if (!nameValue) return;
+
+    newRecipeIngredients.push({
+      amount: newRecipeIngredientsInputAmount.value,
+      name: nameValue,
+      unit: newRecipeIngredientsInputUnit.value as TUnit
+    });
+    newRecipeIngredientsInputAmount.value = '';
+    newRecipeIngredientsInputName.value = '';
+
+    showNewlyAddedIngredients();
+  }
+
+  newRecipeIngredientsInputName.addEventListener('keydown', (event) => {
+    if (event.key !== 'Enter') return;
+    event.preventDefault();
+
+    addIngredientToArray();
+  });
+
+  addIngredientBtn.addEventListener('click', () => {
+    addIngredientToArray();
+  });
+
+  let showNewlyAddedIngredients = () => {
+    newlyAddedIngredients.textContent = '';
+
+    for (let ingredient of newRecipeIngredients) {
+      const ingr = document.createElement('span');
+      ingr.textContent = `${ingredient.amount}${ingredient.unit} ${ingredient.name}`;
+      ingr.classList.add('badge', 'text-bg-success', 'me-2');
+      newlyAddedIngredients.appendChild(ingr);
+    }
   }
 
   newRecipeForm.addEventListener('submit', (event) => {
@@ -53,14 +105,20 @@ export function renderAddNewRecipeView() {
     const newRecipe: IRecipe = {
       name: newRecipeNameInput.value,
       description: newRecipeDescriptionInput.value,
-      category: ['testkategorie'],
+      category: [...newRecipeCategory],
       favorite: false,
       imageUrl: newRecipeImgUrl.value,
-      ingredients: [{ amount: '1', name: 'Spitzkohl', unit: 'Stk.' }, { amount: '2', name: 'Olivenöl', unit: 'EL' }, { amount: '5', name: 'Sojasoße', unit: 'g' }],
-      instructions: 'Einfach loslegen und gut is.'
+      ingredients: [...newRecipeIngredients],
+      instructions: newRecipeInstructionsInput.value
     }
 
     createRecipe(newRecipe);
+
+    /*** UI und State reset */
+    newRecipeForm.reset();
+    newRecipeIngredients = [];
+    newRecipeCategory = [];
+    newlyAddedCategories.textContent = '';
   })
 
 }

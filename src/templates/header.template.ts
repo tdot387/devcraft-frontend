2518361d9f30dd664f2c35a9a1fd79d51c9ba874
Yazan Kassoua <yazan.kassoua@atos.net>
2026-01-12 16:59:54 +0100

    add search input and filter button

--- src/templates/header.template.ts
+++ src/templates/header.template.ts
@@ -9,23 +9,15 @@ export function renderHeaderTemplate(): string {
           </svg>
           <span class="fw-bold">Rezept App</span>
         </a>
-        
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/recipe">Rezepte</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about">Über uns</a>
-            </li>
-          </ul>
+        <div class="d-flex ms-auto w-50">
+          <input type="text" class="form-control w-50 me-2"
+                placeholder="Rezepte oder Kategorien suchen..." aria-label="Search recipes">
+          <button class="btn btn-success">
+            Filter
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+              <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+            </svg>
+          </button>
         </div>
       </div>
     </nav>
